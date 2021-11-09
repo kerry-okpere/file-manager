b54eb488ff80d253e5b918c2fc03cd5c2919a449
kerry-okpere <kerryokpere@gmail.com>
2021-11-09 01:43:57 +0100

    create minmal checkbox

---
+++ src/components/Checkbox/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <input type="checkbox" :checked="modelValue" @change="$emit('update:modelValue', $event.target.checked)">
+</template>
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: Boolean,
+      required: true
+    },
+  },
+  setup() {
+    
+  },
+  emits: ['update:modelValue']
+}
+</script>
