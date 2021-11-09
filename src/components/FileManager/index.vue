7f9f3b5dec438c7df3ca3c9d9406d4963816ebd4
kerry-okpere <kerryokpere@gmail.com>
2021-11-09 22:47:16 +0100

    style file manager

--- src/components/FileManager/index.vue
+++ src/components/FileManager/index.vue
@@ -1,35 +1,56 @@
 <template>
   <transition>
-    <div v-if="visible">
-      <p>{{ name }}</p>
-      <div v-if="error">
+    <div v-if="visible" class="overlay">
+      <header class="overlay-head">
+        <div class="overlay-head-title">
+          <div class="overlay-icon" v-if="parentFolderId !== undefined">
+            <ArrowLeftOutlined :style="{fontSize: '1.3rem', color: '#595959'}"/>
+          </div>
+          <h2>{{ name }}</h2>
+          </div>
+        <div class="overlay-icon">
+          <CloseOutlined @click="$emit('close')" :style="{fontSize: '1.3rem', color: '#595959'}"/>
+        </div>
+      </header>
+      
+      <div v-if="error" class="overlay-msg">
         <p>An error occurred, Please refresh this page</p>
       </div>
-      <div v-else-if="loading">
+
+      <div v-else-if="loading" class="overlay-msg">
         <p>Fetching directory...</p>
       </div>
-      <div v-else-if="folders.length < 1 && files.length < 1">
-        <!-- empty icon -->
-        <p>This directory is empty</p>
+      
+      <div v-else-if="folders.length < 1 && files.length < 1" class="overlay-msg">
+        <div>
+          <FolderOpenOutlined :style="{fontSize: '2rem', color: '#767676'}"/>
+          <p>This directory is empty</p>
+        </div>
       </div>
-      <div v-else>
+      
+      <div v-else class="overlay-body">
         <!-- folders -->
         <DirectoryItem type="folder" v-for="folder in folders" :key="folder.id" :name="folder.name" @click="$emit('change', folder)"/>
         
         <!-- files -->
-        <DirectoryItem v-for="file in files" :key="file.id" :name="file.name" :checked="checked"
+        <DirectoryItem v-for="file in files" :key="file.id" :name="file.name" :checked="isFileSelected()"
           :type="typeOfItem(file.mimeType)" 
           :disabled="typeOfItem(file.mimeType) === 'file:others'"
           :image="typeOfItem(file.mimeType) === 'file:img' ? file.url : ''" 
           @update:checked="$emit('update:checked', {checked: $event, file})" />
           <!-- :checked="typeOfItem(file.mimeType) !== 'file:others' ? isFileSelected() : false" -->
       </div>
+      <footer class="overlay-footer">
+        <Button :title="`Select ${2} Files`"/>
+      </footer>
     </div>
   </transition>
 </template>
 
 <script>
 import DirectoryItem from '@/components/DirectoryItem/index.vue'
+import Button from '@/components/Button/index.vue'
+import { ArrowLeftOutlined, FolderOpenOutlined, CloseOutlined } from '@ant-design/icons-vue'
 
 export default {
   name: 'FileManager',
@@ -61,12 +82,18 @@ export default {
     error: {
       type: Boolean,
       default: false
+    },
+    parentFolderId: {
+      required: true,
+      validator: function(value) {
+        return (value == null || typeof value == 'string')
+      }
     }
   },
   setup(props) {
-    // const isFileSelected = info => {
-    //   return true
-    // }
+    const isFileSelected = file => {
+      return true
+    }
     const typeOfItem = type => {
       if(type === 'image/png' || type === 'image/jpeg'){
         return 'file:img'
@@ -77,13 +104,20 @@ export default {
       return 'file:others'
     }
     return {
-      // isFileSelected,
+      isFileSelected,
       typeOfItem
     }
   },
-  emits: ['update:checked', 'change'],
+  emits: ['update:checked', 'change', 'close'],
   components: {
-    DirectoryItem
+    DirectoryItem,
+    ArrowLeftOutlined,
+    FolderOpenOutlined,
+    CloseOutlined,
+    Button
   }
 }
 </script>
+<style lang="scss" scoped>
+@import './_index.scss';
+</style>
