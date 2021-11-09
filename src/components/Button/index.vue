38465b9d75a2db299f28054104367e30d2598953
kerry-okpere <kerryokpere@gmail.com>
2021-11-09 01:43:39 +0100

    create button component

---
+++ src/components/Button/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <button :class="['btn', type === 'secondary' ? 'btn-secondary' : 'btn-primary']">
+    {{ title }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      default: 'primary',
+      validator: function(value){
+        return ['primary', 'secondary'].includes(value)
+      }
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup() {
+    
+  },
+}
+</script>
