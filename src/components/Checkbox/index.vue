92cbae87fe0f2f58f3b6eb3e4e219616154c63b6
kerry-okpere <kerryokpere@gmail.com>
2021-11-09 22:46:37 +0100

    create checkbox component

--- src/components/Checkbox/index.vue
+++ src/components/Checkbox/index.vue
@@ -1,17 +1,31 @@
 <template>
-  <input type="checkbox" :checked="modelValue" @change="$emit('update:modelValue', $event.target.checked)">
+  <label class="checkbox">
+    <input type="checkbox" :disabled="disabled" :checked="modelValue" @change="$emit('update:modelValue', $event.target.checked)">
+    <span>
+      <CheckOutlined v-show="modelValue" :style="{fontSize: '.65rem', color: '#fff'}" />
+    </span>
+  </label>
 </template>
 <script>
+import { CheckOutlined } from '@ant-design/icons-vue'
+
 export default {
   props: {
     modelValue: {
       type: Boolean,
       required: true
     },
+    disabled: {
+      type: Boolean, 
+      default: false
+    }
   },
-  setup() {
-    
-  },
-  emits: ['update:modelValue']
+  emits: ['update:modelValue'],
+  components: {
+    CheckOutlined
+  }
 }
 </script>
+<style lang="scss" scoped>
+@import './_index.scss';
+</style>
