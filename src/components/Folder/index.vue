819f7cc487dce7748bdafe5e635664fee02c2664
kerry-okpere <kerryokpere@gmail.com>
2021-11-10 10:19:57 +0100

    create a folder component

---
+++ src/components/Folder/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div @click="$emit('click')" class="folder">
+    <div class="folder-title">
+      <div class="folder-icon">
+        <FolderOutlined :style="{fontSize: '1.25rem', color: '#767676'}" />
+      </div>
+      <p>{{ name }}</p>
+    </div>
+    <RightOutlined :style="{fontSize: '1.25rem', color: '#767676'}" />
+  </div>
+</template>
+<script>
+import { FolderOutlined, RightOutlined } from '@ant-design/icons-vue'
+
+export default {
+  name: 'Folder',
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+  },
+  emits: ['click'],
+  components: {
+    FolderOutlined, 
+    RightOutlined
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import './_index.scss';
+</style>
